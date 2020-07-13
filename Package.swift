e44aba99c532f513108be9a935aac4e351cadae7
Danny Sung <danny@dannysung.com>
2020-07-12 19:56:44 -0700

    Updated package references to KituraKommunity

--- Package.swift
+++ Package.swift
@@ -22,9 +22,9 @@ import Foundation
 var kituraNetPackage: Package.Dependency
 
 if ProcessInfo.processInfo.environment["KITURA_NIO"] != nil {
-    kituraNetPackage = .package(url: "https://github.com/IBM-Swift/Kitura-NIO.git", from: "2.3.0")
+    kituraNetPackage = .package(url: "https://github.com/KituraKommunity/Kitura-NIO.git", from: "2.4.100")
 } else {
-    kituraNetPackage = .package(url: "https://github.com/IBM-Swift/Kitura-net.git", from: "2.4.0")
+    kituraNetPackage = .package(url: "https://github.com/KituraKommunity/Kitura-net.git", from: "2.4.100")
 }
 
 let package = Package(
@@ -36,12 +36,12 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.9.0"),
+        .package(url: "https://github.com/KituraKommunity/LoggerAPI.git", from: "1.9.100"),
         .package(url: "https://github.com/apple/swift-log.git", Version("0.0.0") ..< Version("2.0.0")),
         kituraNetPackage,
-        .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", from: "2.0.0"),
-        .package(url: "https://github.com/IBM-Swift/KituraContracts.git", from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/TypeDecoder.git", from: "1.3.0"),
+        .package(url: "https://github.com/KituraKommunity/Kitura-TemplateEngine.git", from: "2.0.100"),
+        .package(url: "https://github.com/KituraKommunity/KituraContracts.git", from: "1.2.100"),
+        .package(url: "https://github.com/KituraKommunity/TypeDecoder.git", from: "1.3.100"),
     ],
     targets: [
         .target(
